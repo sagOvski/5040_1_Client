ef073d58b268e3a988861e488799067a21d2fc2c
sagOvski <ksg.vikas@gmail.com>
2017-10-07 23:10:51 +0200

    Working version of Corba Client compatible with Server with caches and getTopTen() and getUserProfile(). [ToDo] Code clean-up and Client Cache implementation

--- src/TasteProfile/ProfilerClient.java
+++ src/TasteProfile/ProfilerClient.java
@@ -16,16 +16,18 @@ public class ProfilerClient {
 
 		           Scanner c=new Scanner(System.in);
 		           System.out.println("Welcome to the Profiler system:");          		    
-				    for(;;){
+//				    for(;;){
 //				      System.out.println("Enter a:");
 //				      String aa = c.nextLine();
 //				      System.out.println("Enter b:");
 //				      String bb = c.nextLine();
 				    	
-				      int r=addobj.getTimesPlayed("asdf");
-				      System.out.println("The result for Profiler is : "+r);
-				      System.out.println("-----------------------------------");
-		           }
+//				      int r=addobj.getTimesPlayed("SOSPNYF12A6D4F4815");
+		           UserProfile r = addobj.getUserProfile("5fad34f4f0f56d82cd5d09bf0d1cd1dd9d9d7123");
+		           UserProfileSequence seq = addobj.getTopTenUsers();
+		           System.out.println(seq.userProfiles.length);
+		           System.out.println(seq.userProfiles[1].userId);
+//		           }
 		      }
 		      catch (Exception e) {
 		         System.out.println("Hello Client exception: " + e);
